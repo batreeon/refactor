c20b7f018696fd7864860822621ab6b5c22eedc3
batreeon <1340551309@qq.com>
2022-10-22 11:55:03 +0800

    将计算credits综合的逻辑提炼出来，拆分循环，提炼成函数，然后使用内联变量
    Change-Id: I2bfc874b61557581b9bb092431d9380193644571

---
+++ ch1/1_4_5.js
@@ -0,0 +1,72 @@
+// refactor 1_4.js 
+// 将计算credits综合的逻辑提炼出来，拆分循环，提炼成函数，然后使用内联变量
+
+function statement(invoice, plays) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    
+    for (let perf of invoice.performances) {
+        // print line for this order
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+        totalAmount += amountFor(perf);
+    }
+    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
+    return result;
+
+    function amountFor(aPerformance) {
+        let result = 0;
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+        return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        // 计算观众积分
+        let result = Math.max(aPerformance.audience - 30, 0);
+        // add extra credit for every five comedy attendees
+        if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber / 100);
+    }
+
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    }
+}
+
+const invoices = require("./invoces.json");
+const plays = require("./plays.json");
+
+console.log(statement(invoices, plays));
